5281887e2e6276ee13c6b61294195dbce5cb88fc
morga <morgavrieli97@gmail.com>
2021-06-03 17:20:31 +0300

    deal with original_language and vote_count relevant plot code in analyse_data

--- analyse_data.py
+++ analyse_data.py
@@ -1,25 +1,31 @@
 """
 Analyse features: original_language, original_title, overview, vote_count, production_companies
 """
-import splitting_data
-import pandas as pd
+import preprocessing
 import numpy as np
-import json
-import matplotlib as plt
 import matplotlib.pyplot as pplt
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 
-def original_language(df_train):
+def original_language_plot(preprocessing, response):
     """
-    One-Hot feature original_language
-    :param df_train:
+    Plots mean of response={revenue,vote_average} as a function of original_language
+    :param preprocessing:
     :return:
     """
-    df_train = pd.get_dummies(data=df_train, columns=(["original_language"]))
-    df_train.to_csv('C:\\Users\\morga\\IML-Hackathon-2021\\train.csv')
+    fig, ax = pplt.subplots(figsize=(10, 7))
+    languages = pd.unique(preprocessing.df["original_language"])
+    response_means = [preprocessing.df.loc[preprocessing.df['original_language'] == lan][response].mean() for lan in
+                      languages]
+    pplt.bar(languages, response_means)
+    mean_val = sum(response_means) / len(response_means)
+    ax.axhline(mean_val)
+    pplt.savefig(r"C:\Users\morga\IML-Hackathon-2021\plot_revenue_language.png")
+    pplt.clf()
 
 
-def vote_count(df_train):
+def vote_count_plot(preprocessing):
     """
     Plots revenue as a function of vote_count
     :param df_train:
@@ -27,24 +33,16 @@ def vote_count(df_train):
     """
     fig = pplt.figure()
     ax = fig.add_subplot(111)
-    pplt.scatter(df_train[:]["vote_count"], df_train[:]["revenue"])
+    pplt.scatter(preprocessing.df[:]["vote_count"], preprocessing.df[:]["revenue"])
     ax.set_xlabel('vote_count')
     ax.set_ylabel('revenue')
     ax.legend()
-    pplt.show()
-
-
-# def origin_title(df_train):
-#     fig = pplt.figure()
-#     ax = fig.add_subplot(111)
-#     pplt.scatter(df_train[:]["vote_count"], df_train[:]["revenue"])
-#     ax.set_xlabel('vote_count')
-#     ax.set_ylabel('revenue')
-#     ax.legend()
-#     pplt.show()
+    pplt.savefig(r"C:\Users\morga\IML-Hackathon-2021\plot_vote_count.png")
+    pplt.clf()
 
 
 if __name__ == "__main__":
-    df_train = pd.read_csv('C:\\Users\\morga\\IML-Hackathon-2021\\train.csv')
-    original_language(df_train)
-    vote_count(df_train)
+    preprocessing = preprocessing.Preprocessing(r"C:\Users\morga\IML-Hackathon-2021\train.csv")
+    response = "vote_average"
+    original_language_plot(preprocessing, response)
+    vote_count_plot(preprocessing)
--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pplt
+import preprocessing
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 class Preprocessing:
@@ -11,4 +15,20 @@ class Preprocessing:
     def replace_na_in_tagline(self):
         self.df["tagline"] = self.df["tagline"].fillna("")
 
+    def replace_na_in_overview(self):
+        self.df["overview"] = self.df["overview"].fillna("")
 
+    def original_language_feature(self):
+        """
+        One-Hot feature original_language,leave columns of language only for language with higher incomes then the average
+        :param :
+        :return:
+        """
+
+        self.df = pd.get_dummies(data=self.df, columns=(["original_language"]))
+        languages = pd.unique(self.df["original_language"])
+        revenue_means = [self.df.loc[self.df['original_language'] == lan]["revenue"].mean() for lan in languages]
+        mean_val = sum(revenue_means) / len(revenue_means)
+        for one_lan in languages:
+            if one_lan < mean_val:
+                self.df.drop([f"original_language_{one_lan}"])
